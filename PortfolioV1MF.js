9648f090d0af9817ff932e1378df2462f3f07b83
LaloEscualo <122366350+LaloEscualo@users.noreply.github.com>
2023-05-24 19:32:20 +0200

    Sección menu funcionando movíl y desktop

--- PortfolioV1MF.js
+++ PortfolioV1MF.js
@@ -1,3 +1,100 @@
+const inner = document.querySelector(".slidesContainerCarrusel");
+const b = 284;
+
+let cusId = 0;
+class calimatic__Custom__Carousl {
+    slidePx = 0;
+    currentPosition = 0;
+
+    NextSlide(that) {
+        // let inner = that.parentElement.firstElementChild;
+        if (this.currentPosition < inner.childElementCount - 1) {
+            this.slidePx += (b * -1)
+            inner.style.transform = `translateX(${this.slidePx}px)`
+            console.log(inner.style.transform);
+            this.currentPosition++
+            this.currentPosition == inner.childElementCount - 1 ? that.classList.add('disable-button') : that.classList.remove('disable-button');
+            this.currentPosition != 0 ? that.previousElementSibling.classList.remove('disable-button') : that.classList.remove('disable-button');
+        }
+        else {
+            this.slidePx = 0;
+            this.currentPosition = 0;
+            inner.style.transform = `translateX(${this.slidePx}px)`;
+            this.currentPosition++;
+            this.currentPosition == (inner.childElementCount - 1) ? that.classList.remove('disable-button') : that.classList.remove('disable-button');
+            this.currentPosition != 0 ? that.previousElementSibling.style.display = '' : that.classList.add('disable-button');
+        }
+    }
+
+    PrevSlide(that) {
+        // let inner = that.parentElement.firstElementChild;
+        if (this.currentPosition > 0 && this.slidePx < 0) {
+            this.slidePx += (b * 1)
+            inner.style.transform = `translateX(${this.slidePx}px)`
+            this.currentPosition--
+            this.currentPosition == 0 ? that.classList.add('disable-button') : that.classList.remove('disable-button');
+            this.currentPosition != inner.childElementCount ? that.nextElementSibling.classList.remove('disable-button') : that.classList.remove('disable-button');
+        }
+    }
+}
+
+let carouselPipeLine = []
+let carouselObjPipeLine = []
+
+function PrevSlide(that) {
+    let slideWidth = that.parentElement.offsetWidth;
+
+    if (!that.parentElement.hasAttribute('customIndex')) {
+        let allCarousel = document.getElementsByClassName('mainContainerCarrusel');
+        for (let i = 0; i < allCarousel.length; i++) {
+
+            cusId = cusId + 1
+            allCarousel[i].setAttribute('customIndex', `custom${i}${cusId}`)
+        }
+    }
+    if (!carouselPipeLine.includes(that.parentElement.getAttribute('customIndex'))) {
+
+        let obj = that.parentElement.getAttribute('customIndex');
+        carouselPipeLine.push(obj);
+        obj = new calimatic__Custom__Carousl()
+        carouselObjPipeLine.push(obj);
+        obj.PrevSlide(that, slideWidth)
+    }
+    else {
+        let MyIndex = carouselPipeLine.indexOf(that.parentElement.getAttribute('customIndex'));
+        let obj = carouselObjPipeLine[MyIndex];
+        obj.PrevSlide(that, slideWidth)
+    }
+}
+
+function NextSlide(that) {
+
+    let slideWidth = that.parentElement.offsetWidth;
+
+    if (!that.parentElement.hasAttribute('customIndex')) {
+        let allCarousel = document.getElementsByClassName('mainContainerCarrusel');
+        for (let i = 0; i < allCarousel.length; i++) {
+            cusId = cusId + 1
+            allCarousel[i].setAttribute('customIndex', `custom${i}${cusId}`)
+        }
+    }
+    if (!carouselPipeLine.includes(that.parentElement.getAttribute('customIndex'))) {
+        let obj = that.parentElement.getAttribute('customIndex');
+        carouselPipeLine.push(obj);
+        obj = new calimatic__Custom__Carousl()
+        carouselObjPipeLine.push(obj);
+        obj.NextSlide(that, slideWidth)
+
+    }
+    else {
+        let MyIndex = carouselPipeLine.indexOf(that.parentElement.getAttribute('customIndex'));
+        let obj = carouselObjPipeLine[MyIndex];
+        obj.NextSlide(that, slideWidth)
+    }
+}
+
+
+
 //-------------------------------------------- NAVBAR ---------------------------------------------
 
 // MODIFICA LINK NAVBAR CUANDO CLICK
@@ -49,8 +146,8 @@ const shadow = document.querySelector(".mainDivVideo");
 const video = document.querySelector(".containerVideo");
 
 btn.addEventListener("click", function () {
-    if (btn.classList.contains("slide")) {
-        btn.classList.remove("slide");
+    if (btn.classList.contains("slides")) {
+        btn.classList.remove("slides");
         btn.classList.remove("addMargin");
         mainExplainVideo.classList.add("linkShow");
         mainExplainVideo.classList.remove("linkHideCard");
@@ -64,7 +161,7 @@ btn.addEventListener("click", function () {
         video.pause();
     }
     else {
-        btn.classList.add("slide");
+        btn.classList.add("slides");
         btn.classList.add("addMargin");
         mainExplainVideo.classList.add("linkHideCard");
         mainExplainVideo.classList.remove("linkShow");
@@ -96,12 +193,11 @@ const darkBtn = document.querySelector(".darkBtn");
 const sectionSegunda = document.querySelector(".sectionSegunda");
 const mainDivDarkMode = document.querySelector(".mainDivDarkMode");
 const descExplain = document.querySelector(".parrafoReview");
-const cardReview = document.querySelector(".cardReview");
-const ProjectName = document.querySelector(".ProjectNameReview");
+const projectName = document.querySelector(".projectNameReview");
 const spanDark = document.querySelector(".spanDark");
 const spanLigth = document.querySelector(".spanLigth");
-const prevBtnDark = document.querySelector(".prev-btn");
-const nextBtnDark = document.querySelector(".next-btn");
+const prevBtnDark = document.querySelector(".prev");
+const nextBtnDark = document.querySelector(".next");
 
 const scrollXReview = document.querySelectorAll(".itemScrollXReview");
 scrollXReview.forEach(function (review) {
@@ -122,8 +218,7 @@ darkBtn.addEventListener("click", function () {
     mainDivDarkMode.classList.toggle("darkBgCard");
     darkBtn.classList.toggle("darkBgCard");
     descExplain.classList.toggle("fontExplainProject");
-    cardReview.classList.toggle("ligthFont");
-    ProjectName.classList.toggle("ligthFont");
+    projectName.classList.toggle("ligthFont");
     if (window.innerWidth <= 992) {
         spanLigth.classList.toggle("btnShow");
         spanDark.classList.toggle("btnHide");
@@ -134,59 +229,55 @@ darkBtn.addEventListener("click", function () {
 
 // CARRUSEL -----------------------------------------------------------------------------
 
-const carousel = document.querySelector(".carousel");
-const carouselContainer = document.querySelector(".carousel-container");
-const carouselImages = document.querySelectorAll(".carousel .img");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+// const prevButton = document.querySelector(".prev");
+// const nextButton = document.querySelector(".next");
 
-const thirdReview = document.querySelector(".thirdReview");
-const secondReview = document.querySelector(".secondReview");
+// const thirdReview = document.querySelector(".thirdReview");
+// const secondReview = document.querySelector(".secondReview");
 
-const nextBtn = document.querySelector(".next-btn");
+// const nextBtn = document.querySelector(".next-btn");
 
 
-let counter = 0;
-const size = carouselContainer.clientWidth;
+// let counter = 0;
 
 
-window.addEventListener("load", function () {
-    prevButton.classList.add("hideBtn");
-});
+// window.addEventListener("load", function () {
+//     prevButton.classList.add("hideBtn");
+// });
 
-nextButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    secondReview.classList.add("overVisible");
-    thirdReview.classList.add("overVisible");
-    if (counter >= carouselImages.length - 1) return;
-    carousel.style.transition = "transform 0.9s ease-in-out";
-    counter++;
-    if (counter > carouselImages.length - 2) {
-        nextButton.classList.add("hideBtn");
-    } else if (counter = carouselImages.length - 2) {
-        prevButton.classList.remove("hideBtn");
-    }
-    carousel.style.transform = `translateX(${-size * counter - 2}px)`;
-    nextButton.addEventListener("click", (e) => {
-        carousel.style.transform = `translateX(${-size * counter - 4}px)`;
-    });
-});
+// nextButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     secondReview.classList.add("overVisible");
+//     thirdReview.classList.add("overVisible");
+//     if (counter >= carouselImages.length - 1) return;
+//     carousel.style.transition = "transform 0.9s ease-in-out";
+//     counter++;
+//     if (counter > carouselImages.length - 2) {
+//         nextButton.classList.add("hideBtn");
+//     } else if (counter = carouselImages.length - 2) {
+//         prevButton.classList.remove("hideBtn");
+//     }
+//     carousel.style.transform = `translateX(${-size * counter - 2}px)`;
+//     nextButton.addEventListener("click", (e) => {
+//         carousel.style.transform = `translateX(${-size * counter - 4}px)`;
+//     });
+// });
 
-prevButton.addEventListener("click", () => {
+// prevButton.addEventListener("click", () => {
 
-    if (counter <= 0) return;
-    carousel.style.transition = "transform 0.9s ease-in-out";
-    counter--;
-    if (counter <= carouselImages.length - 2) {
-        nextButton.classList.remove("hideBtn");
-    } if (counter <= carouselImages.length - 3) {
-        prevButton.classList.add("hideBtn");
-    }
-    carousel.style.transform = `translateX(${-size * counter - 2}px)`;
-    prevButton.addEventListener("click", (e) => {
-        carousel.style.transform = `translateX(${-size * counter}px)`;
-    });
-});
+//     if (counter <= 0) return;
+//     carousel.style.transition = "transform 0.9s ease-in-out";
+//     counter--;
+//     if (counter <= carouselImages.length - 2) {
+//         nextButton.classList.remove("hideBtn");
+//     } if (counter <= carouselImages.length - 3) {
+//         prevButton.classList.add("hideBtn");
+//     }
+//     carousel.style.transform = `translateX(${-size * counter - 2}px)`;
+//     prevButton.addEventListener("click", (e) => {
+//         carousel.style.transform = `translateX(${-size * counter}px)`;
+//     });
+// });
 
 
 
@@ -274,25 +365,28 @@ const menuArray =
             name: "Lasagna",
             title: "Lunch",
             category: "Lunch",
-            price: 15.35,
+            price: 15,
+            cent: 35,
             img: "images/lasaña.avif",
-            desc: "Thin layers of pasta dough, filled with ricotta cheese, spinach,, and then baked in a tomato sauce with melted mozzarella."
+            desc: "Thin layers of pasta dough, filled with ricotta cheese, spinach, and then baked in a tomato sauce with melted mozzarella."
         },
         {
             id: 2,
-            name: "Pasta Marinara",
+            name: "Pastas",
             title: "Lunch",
             category: "Lunch",
-            price: 10.35,
+            price: 10,
+            cent: 35,
             img: "images/pastaMarinara1.avif",
             desc: "Dish of spaghetti pasta cooked with a tomato sauce, garlic, olive oil, and basil."
         },
         {
             id: 3,
-            name: "Margherita Pizza",
+            name: "Pizzas",
             title: "Lunch",
             category: "Lunch",
-            price: 25.35,
+            price: 25,
+            cent: 35,
             img: "images/PizzaMargarita.avif",
             desc: "Classic Italian pizza made with thin crust, tomato sauce, mozzarella cheese, and fresh basil."
         },
@@ -301,7 +395,8 @@ const menuArray =
             name: "Minestrone",
             title: "Lunch",
             category: "Lunch",
-            price: 5.35,
+            price: 5,
+            cent: 35,
             img: "images/Minestrone.avif",
             desc: "Traditional Italian soup made with tomatoes, onions, garlic, basil, and beef broth."
         },
@@ -310,16 +405,18 @@ const menuArray =
             name: "Risotto",
             title: "Dinner",
             category: "Dinner",
-            price: 12.15,
+            price: 12,
+            cent: 35,
             img: "images/Rissoto.avif",
             desc: "Creamy Italian risotto dish made with Arborio rice, mushrooms, and parmesan cheese."
         },
         {
             id: 6,
             name: "Tiramisu",
-            title: "Dessert",
-            category: "Dessert",
-            price: 12.15,
+            title: "Desserts",
+            category: "Desserts",
+            price: 12,
+            cent: 35,
             img: "images/Tiramisu.avif",
             desc: "Italian dessert made with ladyfingers soaked in a liqueur coffee mixture, layered with a sweetened mascarpone cheese."
         },
@@ -328,33 +425,45 @@ const menuArray =
             name: "House Wine",
             title: "Wines",
             category: "Wines",
-            price: 5.35,
+            price: 5,
+            cent: 35,
+            img: "images/Minestrone.avif",
+            desc: "Rich, full-bodied red with notes of blackberry, leather, and vanilla on the finish."
+        },
+        {
+            id: 8,
+            name: "House Wine",
+            title: "Wines",
+            category: "Wines",
+            price: 5,
+            cent: 35,
             img: "images/Minestrone.avif",
             desc: "Rich, full-bodied red with notes of blackberry, leather, and vanilla on the finish."
         },
     ];
 
-const sectionCenter = document.querySelector(".div-central");
+const sectionCenter = document.querySelector(".divCentralMenu");
 const sectionBtns = document.querySelector(".btnsMenu");
+
 
 window.addEventListener("DOMContentLoaded", function () {
     displayMenuItems(menuArray);
     displayMenuButtons();
 });
+
 function displayMenuItems(menuArray) {
     const displayMenu = menuArray.map(function (part) {
-        return `<div class="">
+        return `<div class="divImageMenu col-auto">
         <img class="imgPlato"
             src=${part.img}
             alt="">
     </div>
-    <div class="mainDivMenu">
-        <div>
+    <div class="mainDivMenu col-6">
+        <div class="mainDivPlato">
             <div class="titlePlato">
                 <h4 class="namePlato">${part.name}</h4>
-                <h4 class="price">${part.price} $</h4>
+                <h4 class="price">$${part.price}<sup class"cents"> ${part.cent}</h4></sup>
             </div>
-            <hr class="underlinePlato">
             <p class="textPlato">${part.desc}</p>
         </div>
     </div>`
@@ -372,7 +481,7 @@ function displayMenuButtons() {
         ["All"]
     );
     const displayBtns = newArrayBtns.map(function (category) {
-        return `<button class="filter-btn" data-id=${category}>${category}</button>`
+        return `<button class="filter-btn changeBorderBtn" data-id=${category}>${category}</button>`
     })
         .join("");
     sectionBtns.innerHTML = displayBtns;
@@ -392,10 +501,17 @@ function displayMenuButtons() {
             else {
                 displayMenuItems(menuByCategory);
             }
-
+        });
+        btn.addEventListener("click", function () {
+            filterBtns.forEach(function (btn) {
+                btn.classList.remove("changeBorderBtn");
+            });
+            this.classList.add("changeBorderBtn");
         });
     });
 }
+
+
 
 //---------------------------------------------- ACCORDION ---------------------------------------------
 
